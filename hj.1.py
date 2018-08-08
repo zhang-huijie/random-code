8bd80a0da413cc19d240b07c4825ac2b806a5c4b
15274977937 <42201006+15274977937@users.noreply.github.com>
2018-08-08 02:39:48 -0500

    Add files via upload

---
+++ hj.1.py
@@ -0,0 +1,11 @@
+
+import sys
+from PyQt5.QtWidgets import QApplication ,QWidget
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    w = QWidget()
+    w.resize(500, 500)
+    w.move(300 ,300)
+    w.setWindowTitle('Hello world')
+    w.show()
+    sys.exit(app.exec_())
